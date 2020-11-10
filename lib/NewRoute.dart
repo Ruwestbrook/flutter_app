a659b7db17b726fed84e6faa9e7380a37531f6ee
russell <igruwestbrook@gmail.com>
2020-11-10 23:00:58 +0800

    Initial commit

---
+++ lib/NewRoute.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class NewRoute extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title:Text( "new route"),
+      ),
+      body: Center(
+        child: Text("this is new route"),
+      ),
+    );
+  }
+
+}
